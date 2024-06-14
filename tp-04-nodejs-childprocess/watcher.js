9bf793120358492ff341058f411e403124608c61
Alexandre AGOSTINI <a.agostini.fr@gmail.com>
2024-06-14 13:59:55 +0200

    +tp childProcess

---
+++ tp-04-nodejs-childprocess/watcher.js
@@ -0,0 +1,21 @@
+import { spawn } from 'node:child_process'
+
+const [node, _, file] = process.argv
+
+// exec('dazdza', (error, out, err) => {
+//     console.log({
+//         error,
+//         out,err
+//     })
+// })
+
+const pr = spawn('dir', [], {shell: true})
+pr.stdout.on('data', (data) => {
+    console.log(data.toString('utf8'))
+})
+pr.stderr.on('data', (data) => {
+    console.log(data.toString('utf8'))
+})
+pr.on('close', (code) => {
+    console.log('Process exited: ' + code)
+})
