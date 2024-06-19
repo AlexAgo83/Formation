b231a5e9ae519b8d813728ef684965920c6aaa8b
Alexandre AGOSTINI <a.agostini.fr@gmail.com>
2024-06-19 13:31:28 +0200

    +react components

--- tp-09-react/src/App.jsx
+++ tp-09-react/src/App.jsx
@@ -1,5 +1,34 @@
+
+const title = "Test 1"
+const tods = [
+  "test1",
+  "test2",
+  "test3",
+  "test4",
+  "test5"
+]
+
+const handleClick = (e) => {
+  console.log('click')
+}
+function Title({color, children}) {
+  return <h1 onClick={handleClick} 
+    name="title" 
+    className="title" 
+    style={{color: color}}>{children ? children : title}</h1>
+}
+
 function App() {
-  return "Hello World!"
+  return <>
+      <Title color="green"/>
+      <Title color="red">Test 2</Title>
+      <p name="content" className="content">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab modi reiciendis provident dolor sunt vero quaerat ipsa, quos delectus illo eum similique consectetur a animi maxime ducimus aperiam odio laborum?
+      </p>
+      <ul>
+        {tods.map((tod, index) => <li key={index}>{tod}</li>)}
+      </ul>
+    </>
 }
 
 export default App
