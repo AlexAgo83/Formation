4119989efee512f9905adc25e21634a360ba8418
Alexandre AGOSTINI <a.agostini.fr@gmail.com>
2024-06-19 15:20:09 +0200

    +react form

--- tp-09-react/src/App.jsx
+++ tp-09-react/src/App.jsx
@@ -1,33 +1,86 @@
+import { useState } from "react"
 
 const title = "Test 1"
 const tods = [
   "test1",
   "test2",
-  "test3",
-  "test4",
-  "test5"
+  "test3"
 ]
 
-const handleClick = (e) => {
+const handleClickTitle = (e) => {
   console.log('click')
 }
 function Title({color, children}) {
-  return <h1 onClick={handleClick} 
+  return <h1 onClick={handleClickTitle} 
     name="title" 
     className="title" 
     style={{color: color}}>{children ? children : title}</h1>
 }
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount((count) => count + 1)
+  }
+
+  const [user, setUser] = useState({
+    firstname: '',
+    age: 0,
+    check: false
+  })
+
+  const incrementAge = (e) => {
+    e.preventDefault()
+    setUser({...user, age: user.age + 1})
+  }
+  const resetUser = () => {
+    e.preventDefault()
+    setUser({firstname: '', age: 0})
+  }
+
+  const handleChangeName = (e) => {
+    setUser({...user, firstname: e.target.value})
+  }
+  const handleChangeAge = (e) => {
+    setUser({...user, age: parseInt(e.target.value)})
+  }
+  const handleChangeCheck = (e) => {
+    setUser({...user, check: e.target.checked})
+  }
+
   return <>
       <Title color="green"/>
       <Title color="red">Test 2</Title>
+
       <p name="content" className="content">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab modi reiciendis provident dolor sunt vero quaerat ipsa, quos delectus illo eum similique consectetur a animi maxime ducimus aperiam odio laborum?
       </p>
       <ul>
         {tods.map((tod, index) => <li key={index}>{tod}</li>)}
       </ul>
+
+      <p>Compteur: {count} <button onClick={increment}>Click</button></p>
+
+      <form>
+        <p>
+          <input type="text" 
+            name="firstname" 
+            value={user.firstname}
+            onChange={handleChangeName} />
+
+          <input type="text" 
+            name="age" 
+            value={user.age} 
+            onChange={handleChangeAge}/>
+
+          <input type="checkbox" 
+            value={user.check} 
+            onChange={handleChangeCheck}/>
+        </p>
+        <button onClick={incrementAge}>Increment age</button>     
+        <button onClick={resetUser}>Reset</button>   
+      </form>
     </>
 }
 
