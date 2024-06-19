38d2e185b212451bee41064ecc203d43e750c2e2
Alexandre AGOSTINI <a.agostini.fr@gmail.com>
2024-06-19 12:44:24 +0200

    +vite env variable

--- tp-07-vite-vanilla-js/main.js
+++ tp-07-vite-vanilla-js/main.js
@@ -1,5 +1,7 @@
-import './style.scss'
+import './style.css'
 
 import('./counter.js').then((module) => {
   module.setupCounter(document.querySelector('button'))
 })
+
+console.log(import.meta.env.VITE_TEST_ENDPOINT)
