cf8558dd902455ed3533bc64d9831b89fe2f7319
Alexandre AGOSTINI <a.agostini.fr@gmail.com>
2024-06-22 15:28:29 +0200

    +error boundary

--- tp-16-blog/src/App.jsx
+++ tp-16-blog/src/App.jsx
@@ -4,6 +4,8 @@ import { Home } from "./pages/Home"
 import { Single } from "./pages/Single"
 import { Contact } from "./pages/Contact"
 import { NotFound } from "./pages/NotFound"
+import { ErrorBoundary } from "react-error-boundary";
+import { Alert } from './components/Alert'
 
 function App() {
   const { page, param } = useHashNavigation();
@@ -12,9 +14,15 @@ function App() {
   return <>
     <Headers page={page} />
     <div className="container my-3">
-      {pageContent}
+      <ErrorBoundary FallbackComponent={PageError}>
+        {pageContent}
+      </ErrorBoundary>
     </div>
   </>
+}
+
+function PageError({error}) {
+  return <Alert type="danger">{error.toString()}</Alert>
 }
 
 function getPageContent(page, param) {
