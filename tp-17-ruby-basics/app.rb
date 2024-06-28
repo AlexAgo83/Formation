49c54ba5523554ddc94113aa040eede39d3ac822
Alexandre AGOSTINI <a.agostini.fr@gmail.com>
2024-06-28 21:31:59 +0200

    ruby test basics

--- tp-17-ruby-basics/app.rb
+++ tp-17-ruby-basics/app.rb
@@ -1,2 +1,7 @@
-puts "Hello"
-puts "World".reverse
+# puts "Hello"
+# puts "World".reverse
+# varHello = "World"
+# varHello = "Hello #{varHello * 2}"
+print "Enter your name: "
+name = gets.chomp
+puts "Hello #{name}"
